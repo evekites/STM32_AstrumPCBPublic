34bb9236f1030dd4aabe8e1aea0fb45cd8881e60
evekites <erik@verberne.net>
2023-05-22 21:28:20 +0200

    Add project

---
+++ 024 AP Gove GPIO LEDs/Core/Src/main.c
@@ -0,0 +1,36 @@
+/* USER CODE BEGIN 2 */
+#define TIME_ON 1000
+#define TIME_OFF 500
+void LED_on()
+{
+  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_SET);
+}
+
+void LED_RED_off()
+{
+  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
+}
+
+void LED_RED_set(GPIO_PinState LED_Status)
+{
+  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, LED_Status);
+}
+/* USER CODE END 2 */
+
+/* USER CODE BEGIN WHILE */
+while (1)
+{
+  LED_RED_on();
+  HAL_Delay(TIME_ON);
+  LED_RED_off();
+  HAL_Delay(TIME_OFF);
+  LED_RED_set(GPIO_PIN_SET);
+  HAL_Delay(TIME_ON);
+  LED_RED_set(GPIO_PIN_RESET);
+  HAL_Delay(TIME_OFF);
+
+  /* USER CODE END WHILE */
+
+  /* USER CODE BEGIN 3 */
+}
+/* USER CODE END 3 */
