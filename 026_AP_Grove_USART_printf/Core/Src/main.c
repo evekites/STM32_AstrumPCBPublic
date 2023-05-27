9cc33fa8eabf210cf7cbf78b11ac422114196bfa
evekites <erik@verberne.net>
2023-05-27 22:40:32 +0200

    Change

--- 026_AP_Grove_USART_printf/Core/Src/main.c
+++ 026_AP_Grove_USART_printf/Core/Src/main.c
@@ -9,22 +9,8 @@ int _write(int file, char *ptr, int len)
 /* USER CODE BEGIN WHILE */
 while (1)
 {
-  char buf[1];
-  buf[0] = 13; // '\r'	Carriage Return
-  HAL_UART_Transmit(&huart1, buf, sizeof(buf), 10000);
-  HAL_Delay(200);
-  buf[0] = 10; // '\n'	Linefeed
-  HAL_UART_Transmit(&huart1, buf, sizeof(buf), 10000);
-  HAL_Delay(200);
-  //'A' = 65
-  //'Z' = 90
-  for (char iCounter = 65; iCounter <= 90; iCounter++)
-  {
-    buf[0] = iCounter;
-    HAL_UART_Transmit(&huart1, buf, sizeof(buf), 10000);
-    HAL_Delay(200);
-  }
-
+  printf("Hello World\r\n");
+  HAL_Delay(500);
   /* USER CODE END WHILE */
 
   /* USER CODE BEGIN 3 */
