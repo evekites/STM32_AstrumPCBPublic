b05265d1577e9f7f8ca9464f63ca3a273fb26d4e
evekites <erik@verberne.net>
2023-06-20 23:14:29 +0200

    Moved counter to Interrupthandler

--- 040_AP_GPIO_ROTARY_encoder_INT/Core/Src/main.c
+++ 040_AP_GPIO_ROTARY_encoder_INT/Core/Src/main.c
@@ -29,23 +29,15 @@ int _write(int file, char *ptr, int len)
 
 /* USER CODE BEGIN WHILE */
 RotaryTransition = HAL_GPIO_ReadPin(ROTARY1_GPIO_Port, ROTARY1_Pin) << 3 | HAL_GPIO_ReadPin(ROTARY2_GPIO_Port, ROTARY2_Pin) << 2 | HAL_GPIO_ReadPin(ROTARY1_GPIO_Port, ROTARY1_Pin) << 1 | HAL_GPIO_ReadPin(ROTARY2_GPIO_Port, ROTARY2_Pin);
+int iPreviousTeller = iTeller;
 while (1)
 {
-  // CW Transition
-  if (RotaryPosition == 4)
+  if (iPreviousTeller != iTeller)
   {
     HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
-    RotaryPosition = 0;
-    iTeller++;
-    printf("Teller: %d\r\n", iTeller);
-  }
-  // CCW Transition
-  else if (RotaryPosition == -4)
-  {
-    HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
-    RotaryPosition = 0;
-    iTeller--;
-    printf("Teller: %d\r\n", iTeller);
+    iPreviousTeller = iTeller;
+    printf("Teller: %d", iTeller);
+    HAL_Delay(100);
   }
   /* USER CODE END WHILE */
 
@@ -59,5 +51,15 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
   RotaryCurrentState = (HAL_GPIO_ReadPin(ROTARY1_GPIO_Port, ROTARY1_Pin) << 1) | HAL_GPIO_ReadPin(ROTARY2_GPIO_Port, ROTARY2_Pin);
   RotaryTransition = (RotaryTransition << 2) | RotaryCurrentState;
   RotaryPosition = RotaryPosition + states[RotaryTransition & 0x0F];
+  if (RotaryPosition == 4)
+  {
+    iTeller++;
+    RotaryPosition = 0;
+  }
+  if (RotaryPosition == -4)
+  {
+    iTeller--;
+    RotaryPosition = 0;
+  }
 }
 /* USER CODE END 4 */
